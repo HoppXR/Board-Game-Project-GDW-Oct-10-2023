5ee84df82d72edbe4abe9fbecd7d1c3ae0c18c80
Dean <40185661+DeanSatine@users.noreply.github.com>
2023-10-11 01:44:41 -0400

    Test 3

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dice : MonoBehaviour
+{
+    private Sprite[] diceSides;
+    private SpriteRenderer rend;
+    private int whosTurn = 1;
+    private bool couroutineAllowed = true;
+
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        diceSides = Resources.LoadAll<Sprite>("DiceSides/");
+        rend.sprite = diceSides[5];
+
+    }
+    
+    private void OnMouseDown()
+    {
+        if (!GameControl.gameOver && couroutineAllowed)
+            StartCoroutine("RollTheDice");
+    }
+
+    private IEnumerator RollTheDice()
+    {
+        couroutineAllowed = false;
+        int randomDiceSide = 0;
+        for (int i = 0; i <= 20; i++)
+        {
+
+            randomDiceSide = Random.Range(0, 6);
+            rend.sprite = diceSides[randomDiceSide];
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        Game.Control.diceSideThrown = randomDiceSide + 1;
+        if (whosTurn == 1)
+        {
+            Game.Control.MovePlayer(1);
+        }
+        else if (whosTurn == -1)
+        {
+            GameControl.MovePlayer(2);
+        }
+        whosTurn *= -1;
+        couroutineAllowed |= true;
+    }
+}
