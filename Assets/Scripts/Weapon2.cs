0b9d100c912b7692cfa7990811919529849c3ce4
Dean <40185661+DeanSatine@users.noreply.github.com>
2023-10-12 21:24:46 -0400

    Latest Version of the Board

--- Assets/Scripts/Weapon2.cs
+++ Assets/Scripts/Weapon2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Weapon : MonoBehaviour
+public class Weapon2 : MonoBehaviour
 {
 
     public int value;
@@ -10,18 +10,18 @@ public class Weapon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void OnTriggerEnter2D(Collider2D other) 
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player1"))
+        if (other.gameObject.CompareTag("Player2"))
         {
             Destroy(gameObject);
             WeaponCounter.instance.IncreaseWeapons(value);
